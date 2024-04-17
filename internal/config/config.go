ed83d0e094e00230eda72fcece3e863b4d8a7a57
Dmitry Atapin <dmitry.atapin.83@gmail.com>
2024-04-17 18:32:48 +0300

    acl-config-1 State interface removed
    Signed-off-by: Dmitry Atapin <dmitry.atapin.83@gmail.com>

--- internal/config/config.go
+++ internal/config/config.go
@@ -78,37 +78,18 @@ func SetConfig(stub shim.ChaincodeStubInterface) error {
 	return nil
 }
 
-type State interface {
-	// GetState returns the value of the specified `key` from the
-	// ledger. Note that GetState doesn't read data from the Write Set, which
-	// has not been committed to the ledger. In other words, GetState doesn't
-	// consider data modified by PutState that has not been committed.
-	// If the key does not exist in the state database, (nil, nil) is returned.
-	GetState(key string) ([]byte, error)
-
-	// PutState puts the specified `key` and `value` into the transaction's
-	// Write Set as a data-write proposal. PutState doesn't affect the ledger
-	// until the transaction is validated and successfully committed.
-	// Simple keys must not be an empty string and must not start with a
-	// null character (0x00) in order to avoid range query collisions with
-	// composite keys, which internally get prefixed with 0x00 as composite
-	// key namespace. In addition, if using CouchDB, keys can only contain
-	// valid UTF-8 strings and cannot begin with an underscore ("_").
-	PutState(key string, value []byte) error
-}
-
 // SaveConfig saves configuration data to the state using the provided State interface.
 //
 // If the provided cfgBytes slice is empty, the function returns an ErrCfgBytesEmpty error.
 //
 // If there is an error while saving the data to the state, an error is returned with
 // additional information about the error.
-func SaveConfig(state State, cfgBytes []byte) error {
+func SaveConfig(stub shim.ChaincodeStubInterface, cfgBytes []byte) error {
 	if len(cfgBytes) == 0 {
 		return ErrCfgBytesEmpty
 	}
 
-	if err := state.PutState(keyConfig, cfgBytes); err != nil {
+	if err := stub.PutState(keyConfig, cfgBytes); err != nil {
 		return fmt.Errorf("putting config data to state: %w", err)
 	}
 
@@ -124,8 +105,8 @@ func SaveConfig(state State, cfgBytes []byte) error {
 // an error is returned with additional information about the error.
 //
 // If the retrieved configuration data is empty, the function returns an ErrCfgBytesEmpty error.
-func loadRawConfig(state State) ([]byte, error) {
-	cfgBytes, err := state.GetState(keyConfig)
+func loadRawConfig(stub shim.ChaincodeStubInterface) ([]byte, error) {
+	cfgBytes, err := stub.GetState(keyConfig)
 	if err != nil {
 		return nil, fmt.Errorf("loading raw config: %w", err)
 	}
@@ -151,8 +132,8 @@ func fromBytes(cfgBytes []byte) (*proto.ACLConfig, error) {
 }
 
 // GetConfig returns config from state
-func GetConfig(state State) (*proto.ACLConfig, error) {
-	cfgBytes, err := loadRawConfig(state)
+func GetConfig(stub shim.ChaincodeStubInterface) (*proto.ACLConfig, error) {
+	cfgBytes, err := loadRawConfig(stub)
 	if err != nil {
 		return nil, err
 	}
