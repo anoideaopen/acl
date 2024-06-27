a198a8e1b31bf9305af7e43258b81f9465aec8c6
Dmitry Atapin <dmitry.atapin.83@gmail.com>
2024-06-27 18:47:59 +0300

    tests fixed
    Signed-off-by: Dmitry Atapin <dmitry.atapin.83@gmail.com>

--- cc/verify.go
+++ cc/verify.go
@@ -2,7 +2,6 @@ package cc
 
 import (
 	"crypto/ed25519"
-
 	aclproto "github.com/anoideaopen/acl/proto"
 	"github.com/anoideaopen/foundation/keys/eth"
 	pb "github.com/anoideaopen/foundation/proto"
