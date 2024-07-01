68615b75fad301b4f737f281ac6ea93acbfdcd39
Yuriy Andreev <yuriy.andreev@atme.com>
2024-07-01 11:53:44 +0300

    added negative test and response from getAccountInfo, checkAddress, checkKeys
    Signed-off-by: Yury Andreev <andreevym@gmail.com>

--- cc/get_accounts_info.go
+++ cc/get_accounts_info.go
@@ -24,24 +24,8 @@ func (c *ACL) getAccountInfoHandlers() map[string]func(shim.ChaincodeStubInterfa
 
 func (c *ACL) GetAccountsInfo(stub shim.ChaincodeStubInterface, _ []string) peer.Response {
 	responses := make([]peer.Response, 0)
-	for _, b := range stub.GetArgs()[1:] {
-		var args []string
-		err := json.Unmarshal(b, &args)
-		if err != nil {
-			return shim.Error(fmt.Sprintf("unmarshal args failed '%s': %s", string(b), err))
-		}
-
-		if len(args) < 2 {
-			return shim.Error(fmt.Sprintf("not enough arguments '%s'", string(b)))
-		}
-
-		method := args[0]
-		methodArgs := args[1:]
-		handler, ok := c.getAccountInfoHandlers()[method]
-		if !ok {
-			responses = append(responses, shim.Error(fmt.Sprintf("failed get accounts info: unknown method '%s'", method)))
-		}
-		response := handler(stub, methodArgs)
+	for _, bytes := range stub.GetArgs()[1:] {
+		response := c.handleGetAccountsInfoItem(stub, bytes)
 		responses = append(responses, response)
 	}
 
@@ -50,4 +34,24 @@ func (c *ACL) GetAccountsInfo(stub shim.ChaincodeStubInterface, _ []string) peer
 		return shim.Error(fmt.Sprintf("failed get accounts info: marshal GetAccountsInfoResponse: %s", err))
 	}
 	return shim.Success(bytes)
+}
+
+func (c *ACL) handleGetAccountsInfoItem(stub shim.ChaincodeStubInterface, b []byte) peer.Response {
+	var args []string
+	err := json.Unmarshal(b, &args)
+	if err != nil {
+		return shim.Error(fmt.Sprintf("unmarshal args failed '%s': %s", string(b), err))
+	}
+
+	if len(args) < 2 {
+		return shim.Error(fmt.Sprintf("not enough arguments '%s'", string(b)))
+	}
+
+	method := args[0]
+	methodArgs := args[1:]
+	handler, ok := c.getAccountInfoHandlers()[method]
+	if !ok {
+		return shim.Error(fmt.Sprintf("failed get accounts info: unknown method '%s'", method))
+	}
+	return handler(stub, methodArgs)
 }
