8c98fcdf3cfe74a9de7cb0dce9403c5a042880e8
Dmitry Atapin <dmitry.atapin.83@gmail.com>
2024-07-08 14:23:28 +0300

    old tests added
    Signed-off-by: Dmitry Atapin <dmitry.atapin.83@gmail.com>

--- tests/integration/cmn/client/acl.go
+++ tests/integration/cmn/client/acl.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"github.com/anoideaopen/acl/cc"
 	"github.com/anoideaopen/acl/tests/common"
@@ -15,6 +16,7 @@ import (
 	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
 	"google.golang.org/protobuf/proto"
+	"strconv"
 	"time"
 )
 
@@ -57,6 +59,45 @@ func addToList(
 	CheckUserInList(network, peer, listType, user)
 }
 
+// DelFromBlackList adds user to a black list
+func DelFromBlackList(network *nwo.Network, peer *nwo.Peer, orderer *nwo.Orderer, user *client.UserFoundation) {
+	delFromList(network, peer, orderer, cc.BlackList, user)
+}
+
+// DelFromGrayList adds user to a gray list
+func DelFromGrayList(network *nwo.Network, peer *nwo.Peer, orderer *nwo.Orderer, user *client.UserFoundation) {
+	delFromList(network, peer, orderer, cc.GrayList, user)
+}
+
+func delFromList(
+	network *nwo.Network,
+	peer *nwo.Peer,
+	orderer *nwo.Orderer,
+	listType cc.ListType,
+	user *client.UserFoundation,
+) {
+	sess, err := network.PeerUserSession(peer, "User1", commands.ChaincodeInvoke{
+		ChannelID: cmn.ChannelAcl,
+		Orderer:   network.OrdererAddress(orderer, nwo.ListenPort),
+		Name:      cmn.ChannelAcl,
+		Ctor: cmn.CtorFromSlice([]string{
+			common.FnDelFromList,
+			user.AddressBase58Check,
+			listType.String(),
+		}),
+		PeerAddresses: []string{
+			network.PeerAddress(network.Peer("Org1", "peer0"), nwo.ListenPort),
+			network.PeerAddress(network.Peer("Org2", "peer0"), nwo.ListenPort),
+		},
+		WaitForEvent: true,
+	})
+	Expect(err).NotTo(HaveOccurred())
+	Eventually(sess, network.EventuallyTimeout).Should(gexec.Exit(0))
+	Expect(sess.Err).To(gbytes.Say("Chaincode invoke successful. result: status:200"))
+
+	CheckUserNotInList(network, peer, listType, user)
+}
+
 // CheckUserInList - checks if user in gray or black list
 func CheckUserInList(
 	network *nwo.Network,
@@ -89,6 +130,44 @@ func CheckUserInList(
 
 		if !((resp.Account.GetBlackListed() && listType == cc.BlackList) || (resp.Account.GetGrayListed() && listType == cc.GrayList)) {
 			return fmt.Sprintf("Error: expected %s to be added to %s", user.AddressBase58Check, listType.String())
+		}
+
+		return ""
+	}, network.EventuallyTimeout, time.Second).Should(BeEmpty())
+}
+
+// CheckUserNotInList - checks if user in gray or black list
+func CheckUserNotInList(
+	network *nwo.Network,
+	peer *nwo.Peer,
+	listType cc.ListType,
+	user *client.UserFoundation,
+) {
+	Eventually(func() string {
+		sess, err := network.PeerUserSession(peer, "User1", commands.ChaincodeQuery{
+			ChannelID: cmn.ChannelAcl,
+			Name:      cmn.ChannelAcl,
+			Ctor:      cmn.CtorFromSlice([]string{common.FnCheckKeys, user.PublicKeyBase58}),
+		})
+		Eventually(sess, network.EventuallyTimeout).Should(gexec.Exit())
+		if sess.ExitCode() != 0 {
+			return fmt.Sprintf("exit code is %d: %s, %v", sess.ExitCode(), string(sess.Err.Contents()), err)
+		}
+
+		out := sess.Out.Contents()[:len(sess.Out.Contents())-1] // skip line feed
+		resp := &pb.AclResponse{}
+		err = proto.Unmarshal(out, resp)
+		if err != nil {
+			return fmt.Sprintf("failed to unmarshal response: %v", err)
+		}
+
+		addr := base58.CheckEncode(resp.GetAddress().GetAddress().GetAddress()[1:], resp.GetAddress().GetAddress().GetAddress()[0])
+		if addr != user.AddressBase58Check {
+			return fmt.Sprintf("Error: expected %s, received %s", user.AddressBase58Check, addr)
+		}
+
+		if !((!resp.Account.GetBlackListed() && listType == cc.BlackList) || (!resp.Account.GetGrayListed() && listType == cc.GrayList)) {
+			return fmt.Sprintf("Error: expected %s to be deleted from %s", user.AddressBase58Check, listType.String())
 		}
 
 		return ""
@@ -210,4 +289,123 @@ func CheckUserChangedKey(network *nwo.Network, peer *nwo.Peer, newPublicKeyBase5
 
 		return ""
 	}, network.EventuallyTimeout, time.Second).Should(BeEmpty())
+}
+
+// CheckAccountInfo checks account info
+func CheckAccountInfo(
+	network *nwo.Network,
+	peer *nwo.Peer,
+	user *client.UserFoundation,
+	kycHash string,
+	isGrayListed,
+	isBlackListed bool,
+) {
+	Eventually(func() string {
+		sess, err := network.PeerUserSession(peer, "User1", commands.ChaincodeQuery{
+			ChannelID: cmn.ChannelAcl,
+			Name:      cmn.ChannelAcl,
+			Ctor:      cmn.CtorFromSlice([]string{common.FnGetAccInfoFn, user.AddressBase58Check}),
+		})
+		Eventually(sess, network.EventuallyTimeout).Should(gexec.Exit())
+		if sess.ExitCode() != 0 {
+			return fmt.Sprintf("exit code is %d: %s, %v", sess.ExitCode(), string(sess.Err.Contents()), err)
+		}
+
+		out := sess.Out.Contents()[:len(sess.Out.Contents())-1] // skip line feed
+		resp := &pb.AccountInfo{}
+		err = json.Unmarshal(out, resp)
+		if err != nil {
+			return fmt.Sprintf("failed to unmarshal response: %v", err)
+		}
+
+		if resp.GetKycHash() != kycHash {
+			return fmt.Sprintf("kyc check error: expected %s, received %s", kycHash, resp.GetKycHash())
+		}
+
+		if resp.GetGrayListed() != isGrayListed {
+			return fmt.Sprintf("gray list check error error: expected %t, received %t", isGrayListed, resp.GetGrayListed())
+		}
+
+		if resp.GetBlackListed() != isBlackListed {
+			return fmt.Sprintf("black list check error: expected %t, received %t", isBlackListed, resp.GetBlackListed())
+		}
+
+		return ""
+	}, network.EventuallyTimeout, time.Second).Should(BeEmpty())
+}
+
+// SetAccountInfo sets account info
+func SetAccountInfo(
+	network *nwo.Network,
+	peer *nwo.Peer,
+	orderer *nwo.Orderer,
+	user *client.UserFoundation,
+	kycHash string,
+	isGrayListed,
+	isBlackListed bool,
+) {
+	sess, err := network.PeerUserSession(peer, "User1", commands.ChaincodeInvoke{
+		ChannelID: cmn.ChannelAcl,
+		Orderer:   network.OrdererAddress(orderer, nwo.ListenPort),
+		Name:      cmn.ChannelAcl,
+		Ctor: cmn.CtorFromSlice([]string{
+			"setAccountInfo",
+			user.AddressBase58Check,
+			kycHash,
+			strconv.FormatBool(isGrayListed),
+			strconv.FormatBool(isBlackListed),
+		}),
+		PeerAddresses: []string{
+			network.PeerAddress(network.Peer("Org1", "peer0"), nwo.ListenPort),
+			network.PeerAddress(network.Peer("Org2", "peer0"), nwo.ListenPort),
+		},
+		WaitForEvent: true,
+	})
+	Expect(err).NotTo(HaveOccurred())
+	Eventually(sess, network.EventuallyTimeout).Should(gexec.Exit(0))
+	Expect(sess.Err).To(gbytes.Say("Chaincode invoke successful. result: status:200"))
+
+	CheckAccountInfo(network, peer, user, kycHash, isGrayListed, isBlackListed)
+}
+
+// SetKYC sets kyc hash
+func SetKYC(
+	network *nwo.Network,
+	peer *nwo.Peer,
+	orderer *nwo.Orderer,
+	user *client.UserFoundation,
+	kycHash string,
+	validators ...*client.UserFoundation,
+) {
+	ctorArgs := []string{common.FnSetKYC, user.AddressBase58Check, kycHash, client.NewNonceByTime().Get()}
+	validatorMultisignedUser := &client.UserFoundationMultisigned{
+		UserID: "multisigned validators",
+		Users:  validators,
+	}
+
+	pKeys, sMsgsByte, err := validatorMultisignedUser.Sign(ctorArgs...)
+	Expect(err).NotTo(HaveOccurred())
+
+	var sMsgsStr []string
+	for _, sMsgByte := range sMsgsByte {
+		sMsgsStr = append(sMsgsStr, hex.EncodeToString(sMsgByte))
+	}
+
+	ctorArgs = append(append(ctorArgs, pKeys...), sMsgsStr...)
+	sess, err := network.PeerUserSession(peer, "User1", commands.ChaincodeInvoke{
+		ChannelID: cmn.ChannelAcl,
+		Orderer:   network.OrdererAddress(orderer, nwo.ListenPort),
+		Name:      cmn.ChannelAcl,
+		Ctor:      cmn.CtorFromSlice(ctorArgs),
+		PeerAddresses: []string{
+			network.PeerAddress(network.Peer("Org1", "peer0"), nwo.ListenPort),
+			network.PeerAddress(network.Peer("Org2", "peer0"), nwo.ListenPort),
+		},
+		WaitForEvent: true,
+	})
+	Expect(err).NotTo(HaveOccurred())
+	Eventually(sess, network.EventuallyTimeout).Should(gexec.Exit(0))
+	Expect(sess.Err).To(gbytes.Say("Chaincode invoke successful. result: status:200"))
+
+	CheckAccountInfo(network, peer, user, kycHash, false, false)
 }
