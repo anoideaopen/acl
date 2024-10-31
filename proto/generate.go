bf1b1936032930e53f018846eecc6f9bb2f9fc89
pfi79 <fredprtnsk@gmail.com>
2024-10-31 19:59:00 +0300

    delete old shimtest (#54)
    Signed-off-by: Fedor Partanskiy <fredprtnsk@gmail.com>

--- proto/generate.go
+++ proto/generate.go
@@ -1,3 +1,10 @@
 package proto
 
+import "github.com/hyperledger/fabric-chaincode-go/shim"
+
 //go:generate protoc -I=. --go_out=paths=source_relative:. acl-config.proto
+//go:generate counterfeiter -generate
+//counterfeiter:generate -o ../tests/unit/mock/chaincode_stub.go --fake-name ChaincodeStub . chaincodeStub
+type chaincodeStub interface { //nolint:unused
+	shim.ChaincodeStubInterface
+}
